cc519bcee8f1a14e7c8b65d6fb54afab1a219a0f
LedesmaFrancoAgustin <ledesmafranco50@gmail.com>
2024-01-03 23:35:45 -0300

    Primer Commit

---
+++ app.js
@@ -0,0 +1,52 @@
+
+
+let apellido = "Ledesma";
+const edad = 10;
+const dni = 41099153;
+
+/**  Opreaciones Basicas */
+
+let numero1 = 10;
+let numero2 = 5;
+
+let Resultado = numero1 + numero2;
+
+
+/** String */
+
+let nombre = "Franco "
+let apodo = "Pachi"
+
+let cadenaDeTexto = nombre + apodo
+
+/** String Suma */
+
+let numero3 = 5
+let texto = "Pachi"
+
+let cadenaDeTextoString = numero3 + texto
+
+/** Prompt   *******************************************************************/
+
+console.log(cadenaDeTextoString)
+
+/**  Ingresar y mostrar */
+
+let nombrePrompt = prompt("Ingresa tu nombre")
+
+console.log(nombrePrompt)
+
+/** Alert    *******************************************************************/
+
+alert( " Informacion ")
+
+
+/*  Ejemplo   */
+
+const anioNacimiento = parseInt(prompt ("Año de nacimiento  "))
+
+let anioActual = parseInt(prompt ("Año Actual "))
+
+let edadActual =  anioActual - anioNacimiento 
+
+console.log(edadActual)
